573f8ac26ffa3f17002a05a6f9274b6d267850ca
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-15 16:09:43 -0400

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    file = open(f'{file_name}.txt', mode='w', encoding='utf-8')
+    file.write(file_content)
+    file.close()
 
 def append_file(file_name, append_content):
-    pass
+    file = open(f'{file_name}.txt', 'a')
+    file.write(append_content)
+    file.close()
+
 
 def read_file(file_name):
-    pass
+    file = open(f'{file_name}.txt')
+    result = file.read()
+    file.close()
+    return result
